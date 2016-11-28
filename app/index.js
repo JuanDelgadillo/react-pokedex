18105bf82686906bdc26cb6a6216da154d4c998c
Juan Delgadillo <j.mnuelh@gmail.com>
2016-11-27 21:07:29 -0600

    Added basic html layout for Pokemon.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 
 const HelloWorld = (props) => {
     return (
-      <div>Hello world =) </div>
+      <div>
+        <h1>Bulbasaur #001</h1>
+        <h2>Seed Pokémon</h2>
+        <img src="app/assets/images/bulbasaur.jpg" />
+        <h2>Pokédex</h2>
+        <ul>
+          <li><strong>Type</strong> Grass, Poison</li>
+          <li><strong>Height</strong> 2'4" (0.71m)</li>
+          <li><strong>Weight</strong> 15.2 lbs (6.9 kg)</li>
+          <li><strong>Abilities</strong> Overgrow, Chlorophyll</li>
+        </ul>
+      </div>
     )
 }
 
